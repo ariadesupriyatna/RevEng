23205c42decb9c9462f99edf26663fca9c62a542
code27 <88775244+ariadesupriyatna@users.noreply.github.com>
2024-06-05 15:26:03 +0700

    Update xor.c

--- xor.c
+++ xor.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
-void enkripsi_xor(char *teks, int kunci) {
-    while (*teks) {
-        *teks = *teks ^ kunci;
-        teks++;
+void enD(char *t, int k) {
+    while (*t) {
+        *t = *t ^ k;
+        t++;
     }
 }
 
 int main() {
     char kunci[20];
-    char flag[20];
 
-    char sandi_terenkripsi[] = {0x66, 0x7E, 0x68, 0x7F, 0x76, 0x78, 0x72, 0x7F, 0x7C, 0x77, 0x7D, 0x7D, 0};
-    char flag_terenkripsi[] = {0x74, 0x74, 0x7A, 0x7C, 0x7F, 0x7D, 0x7D, 0x7C, 0x63, 0x63, 0x60, 0x61, 0x65, 0x7E, 0};
+    char sT[] = {0x66, 0x7E, 0x68, 0x7F, 0x76, 0x78, 0x72, 0x7F, 0x7C, 0x77, 0x7D, 0x7D, 0};
+    char fT[] = {0x74, 0x74, 0x7A, 0x7C, 0x7F, 0x7D, 0x7D, 0x7C, 0x63, 0x63, 0x60, 0x61, 0x65, 0x7E, 0};
 
     printf("Masukkan kata sandi: ");
     scanf("%s", kunci);
 
-    enkripsi_xor(sandi_terenkripsi, 0x07);
-    enkripsi_xor(flag_terenkripsi, 0x07);
+    enD(sT, 0x07);
+    enD(fT, 0x07);
 
-    if (strcmp(kunci, sandi_terenkripsi) == 0) {
-        printf("Selamat! Bendera: %s\n", flag_terenkripsi);
+    if (strcmp(kunci, sT) == 0) {
+        printf("Selamat! Bendera: %s\n", fT);
     } else {
         printf("Kata sandi salah!\n");
     }
